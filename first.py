97bef9d1f3fa55c437b856d496835f182e6f1933
vinodh sanjeevi <vin.sanjeevi@gmail.com>
2024-09-15 01:23:41 +0530

    testing of name

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
-print("hi")
+def fact(n):
+    f=1
+    for i in range(1, n+1):
+        f*=i
+    return f
+t = fact(5)
+print(t)
+if __name__ == '__main__':
+    print("------------------testing code ------ ")
+    print(fact(4))
+    print(fact(7))
+    print(" ---------------------------------------  ")
--- second.py
+++ second.py
@@ -1,6 +1,2 @@
-l1 = [1,2,3,4,67]
-l2=l1
-l2[0]= 12
-print(l1)
-print(l2)
-print(" ********************* ")
+from first import fact
+print(fact(17))
